a987ee8cf446c35cd4fce7e2a75faf2264f2f495
Lisonechka <elizza402@gmail.com>
2021-07-19 23:01:52 +0300

    Start reglar ex of day2

---
+++ Week3/Day2/Exercises/script1.js
@@ -0,0 +1,38 @@
+5 + "34" //expected result "5 34"
+//actual result "534"
+
+5 - "4" //expected result "NaN"
+//actual result "1"
+
+10 % 5 //expected result 0
+//actual result 0
+
+5 % 10 //expected result 0
+//actual result 5
+
+"Java" + "Script" //expected result "JavaScript"
+//actual result "JavaScript"
+
+" " + " " //expected result  " " 
+//actual result " "
+
+" " + 0  //expected result  " 0" 
+//actual result " 0"
+
+true + true //expected result  2 
+//actual result 2
+
+true + false //expected result  1 
+//actual result 1
+
+false + true //expected result  1 
+//actual result 1
+
+false - true //expected result  -1 
+//actual result -1
+
+3 - 4 //expected result  -1 
+//actual result -1
+
+"Bob" - "bill" //expected result  NaN 
+//actual result NaN
