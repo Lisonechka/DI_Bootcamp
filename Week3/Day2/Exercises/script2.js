2c9a159ca1f4875ef1bac9494f3d44e0fd3734f4
Lisonechka <elizza402@gmail.com>
2021-07-20 00:52:00 +0300

    New exercises

---
+++ Week3/Day2/Exercises/script2.js
@@ -0,0 +1,38 @@
+//Favorite color
+let me = ["my","favorite","color","is","blue"]
+console.log(me.join(' '))
+//or
+let me = ["my","favorite","color","is","blue"];
+let mejoin = me.join(' ');
+console.log(mejoin);
+
+//Mixup
+let str1 = "milk";
+let str2 = "fresh";
+let str2slice = str2.slice(0, 2);
+let str1slice = str1.slice(0, 2);
+//can't solve
+
+//Calculator "+"
+let num1 = prompt('Enter the first number');
+let num2 = prompt('Enter the second number')
+let sum = Number(num1) + Number(num2)
+alert('The sum is ' + sum);
+
+//Calculator "-"
+let num1 = prompt('Enter the first number');
+let num2 = prompt('Enter the second number')
+let subtract = Number(num1) - Number(num2)
+alert('The result is ' + subtract);
+
+//Calculator '*'
+let num1 = prompt('Enter the first number');
+let num2 = prompt('Enter the second number')
+let multiply = Number(num1) * Number(num2)
+alert('The result is ' + multiply);
+
+//Calculator "/"
+let num1 = prompt('Enter the first number');
+let num2 = prompt('Enter the second number')
+let divide = Number(num1) / Number(num2)
+alert('The result is ' + divide);
