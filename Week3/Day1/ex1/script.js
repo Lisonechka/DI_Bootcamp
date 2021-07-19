735ed8568da2813eff7bc0c1f646d38c96c9d0a0
Lisonechka <elizza402@gmail.com>
2021-07-19 23:02:53 +0300

    exercise of day1

---
+++ Week3/Day1/ex1/script.js
@@ -0,0 +1,11 @@
+let addressNumber = "29";
+let addressStreet = "Rainbow"
+let country = "Israel"
+
+let globalAdress = "I live in " + addressStreet + " " + addressNumber + "," + " in " + country;
+console.log(globalAdress)
+
+let birthYear = 1993;
+let futureYear = 2021;
+let futureAge = futureYear - birthYear;
+console.log(`I will be  ${futureAge} in  ${futureYear}`);
