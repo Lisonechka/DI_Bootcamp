a0b34fcd54b140eb9a05ba4e5fe936b5d7236d01
Lisonechka <elizza402@gmail.com>
2021-07-20 10:07:15 +0300

    Added daily chalenge

---
+++ Week3/Day2/challenge/script.js
@@ -0,0 +1,13 @@
+//Exercise 1
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+fruits.sort();
+fruits.push("Kiwi");
+fruits.splice(0, 1);
+fruits.reverse();
+console.log(fruits);
+
+//Exercise 2
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+let favorit = moreFruits[1][1];
+console.log(favorit);
