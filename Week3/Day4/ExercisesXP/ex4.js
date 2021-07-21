6198ff4a3585ecae8d3b3c7e2a9c39e17eb82c28
Lisonechka <elizza402@gmail.com>
2021-07-21 12:03:36 +0300

    Submited ex

--- Week3/Day4/ExercisesXP/ex4.js
+++ Week3/Day4/ExercisesXP/ex4.js
@@ -1,10 +1,11 @@
 let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
-if(users.length = 0) {
+let moreUsers = users.length - 2;
+if(users.length == 0) {
     console.log('no one is online');
-} else if(users.length = 1) {
+} else if(users.length == 1) {
     console.log( users[0] + ' is online');
-} else if(users.length = 2) {
+} else if(users.length == 2) {
     console.log(users[0] + ' and ' + users[1] + ' are online');
-} else(users.length > 2)  {
-    console.log(users[0] + ', ' + users[1] + ' and ' + users.length(2, ) + ' more are omline');
+} else {
+    console.log(users[0] + ', ' + users[1] + ' and ' + moreUsers + ' more are omline');
 }
