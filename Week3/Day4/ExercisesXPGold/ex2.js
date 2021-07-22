9dfa521b43aa5b8147f8f044fbae0035a2348b74
Lisonechka <elizza402@gmail.com>
2021-07-22 10:14:00 +0300

    Added solution

---
+++ Week3/Day4/ExercisesXPGold/ex2.js
@@ -0,0 +1,9 @@
+let grade = prompt('What is your grade?');
+if(grade > 80 && grade <= 90) {
+    console.log('B');
+} else if(grade >=70 && grade <=80) {
+    console.log('C');
+} else {
+    console.log('D');
+}
+    
