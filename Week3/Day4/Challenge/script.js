984a8d4ac813923e0594a1bd4fe7f3e334a36adf
Lisonechka <elizza402@gmail.com>
2021-07-23 23:36:30 +0300

    Solved problem

--- Week3/Day4/Challenge/script.js
+++ Week3/Day4/Challenge/script.js
@@ -2,10 +2,9 @@ let sentence = "The dish is not so bad, I can eat it."
 let wordNot = sentence.indexOf('not');
 let wordBad = sentence.indexOf('bad');
 if(wordBad > wordNot) {
-    let newSentence = sentence.replace("/(not*bad)/", "good");
+    let newSentence = sentence.replace(/not.*bad/, "good");
     console.log(newSentence);
 } else {
     console.log(sentence);
 
 }
-//don't know how to solve problem with "not so bad". RegEx, but how?
