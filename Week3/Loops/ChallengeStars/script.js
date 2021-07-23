dc1f1ece22222598643a526850bd7e189ba9f105
Lisonechka <elizza402@gmail.com>
2021-07-23 23:29:18 +0300

    Added 2 solutions

---
+++ Week3/Loops/ChallengeStars/script.js
@@ -0,0 +1,14 @@
+//Using one loop
+//  for (let i=1, str=''; i<=6; i++) {
+//      str+= '*';
+//      console.log(str);
+//      }
+
+ //two nested for loops
+ let str =' ';
+ for (let i = 1; i <= 6; i++) {
+     for ( let j = 1; j <= 1; j++) {
+        str += '*';
+    }
+   console.log(str);
+}
