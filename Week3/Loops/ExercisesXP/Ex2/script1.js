43bdf25f8b38f8221ff5cd7f6082d762b5037c09
Lisonechka <elizza402@gmail.com>
2021-07-23 19:34:56 +0300

    Added paragraph 5

--- Week3/Loops/ExercisesXP/Ex2/script1.js
+++ Week3/Loops/ExercisesXP/Ex2/script1.js
@@ -1,7 +1,10 @@
 let people = ["Greg", "Mary", "Devon", "James"];
 people.shift();
-people[3] = "Jason";
+people[2] = "Jason";
 people.push("Liza");
 for (let i in people) {
+    if ( i = "Jason") {
+        break
+    }
     console.log(people[i]);
 }
