29e46a33d707cb2449d69f3db710c71753f2f9cd
Lisonechka <elizza402@gmail.com>
2021-07-23 19:28:18 +0300

    Added solution

---
+++ Week3/Loops/ExercisesXP/Ex1/script.js
@@ -0,0 +1,16 @@
+let colors = ["white", "black", "blue", "red", "pink"];
+let number = 1;
+for (let i in colors) {
+ console.log("My #" + number++ + " choice is " + colors[i]);
+}
+
+// //bonus
+// let colors = ["white", "black", "blue", "red", "pink"];
+// let number = 1;
+// let suffix = ["st", "nd", "rd", "th", "th"];
+// let x = suffix++;
+// for (let i in colors) {
+// console.log("My " + number++ +  suffix[x] + " choice is " +  colors[i]);
+
+// }    
+    
