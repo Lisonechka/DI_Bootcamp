372998d03d2509fa0209ca8537635463342b6fde
Lisonechka <elizza402@gmail.com>
2021-07-24 00:35:20 +0300

    Added outcomes

---
+++ Week3/Day2/XPNinja/Evaluation2/script.js
@@ -0,0 +1,11 @@
+let c; //undefinde
+ 
+let a = 34;//if we put a, outcome will be 34
+
+let b = 21; // if we put b, outcome will be 21
+
+a = 2; //if we put a, outcome will be 2
+
+a + b // 23
+
+console.log(3 + 4 + '5'); //outcome 75
