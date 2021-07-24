5d116e1ea0020843cdfebe9f883d1d0329535003
Lisonechka <elizza402@gmail.com>
2021-07-24 20:35:00 +0300

    Added solution, problem with UpperCase

---
+++ Week3/Day2/XPNinja/Boom/script.js
@@ -0,0 +1,39 @@
+let number = prompt('Write a number');
+let o = "o";
+let nO = 'B' + o.repeat(number) + 'm';
+if (number < 2) {
+      alert('Boom');
+   } else if ((number >= 2) && (number %2 !== 0)) {
+      alert(nO);
+  }  else if ((number >= 2) && (number %2 == 0) && (number %5 !== 0)) {
+      alert(nO + '!');
+  }  else if ((number %2 !== 0) && (number %5 == 0)) {
+      alert(nO.toUpperCase()); // Why doesn't work?
+  }  else if ((number %2 == 0) && (number %5 == 0)) {
+     alert(nO.toUpperCase() + '!');
+  }
+
+
+// Try SWITCH
+//  switch(number) {
+//     case number < 2: //1
+//         alert('Boom');
+//       break;
+//     case number >= 2:
+//     case number %2 !== 0: //2
+//         alert(nO)
+//       break;
+//     case number >= 2:
+//     case number %2 == 0:
+//     case number %5 !== 0: //3
+//         alert(nO + '!');
+//       break;
+//     case number %2 !== 0:
+//     case number %5 == 0: //4
+//         alert(nO.toUpperCase())
+//       break;
+//     case number %2 == 0:
+//     case number %5 == 0:
+//         alert(nO.toUpperCase() + '!');
+//       break;
+//  }
