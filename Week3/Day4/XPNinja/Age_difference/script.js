0ab186b418dc5cfb7ebfe4a785e9a9f401069d29
Lisonechka <elizza402@gmail.com>
2021-07-24 21:24:18 +0300

    Added solution, ?conditionals

---
+++ Week3/Day4/XPNinja/Age_difference/script.js
@@ -0,0 +1,6 @@
+let oldYearBirth = 1987;
+let youngYearBirth = 2003;
+let halfAge = (youngYearBirth - oldYearBirth) / 2;
+let yearYoungHalfOlder = youngYearBirth + halfAge;
+ // how to solve with conditionals?
+ 
