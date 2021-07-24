f1893b1d48357260e5470109d0150c726398262a
Lisonechka <elizza402@gmail.com>
2021-07-24 15:36:31 +0300

    ? about two-digit numbers

---
+++ Week3/Day2/XPNinja/AskforNumbers/script.js
@@ -0,0 +1,5 @@
+let numbers = prompt('Write 2 numbers, separated by commas');
+let sum = Number(numbers[0]) + Number(numbers[2]);
+console.log(sum);
+
+//works with single digits, doen't work with two-digit numbers etc.
