076d6d8c34b3c66b443384f3cd99d1f38096fd56
Lisonechka <elizza402@gmail.com>
2021-07-25 22:06:52 +0300

    final solution

--- Week3/Day2/XPNinja/AskforNumbers/script.js
+++ Week3/Day2/XPNinja/AskforNumbers/script.js
@@ -1,5 +1,7 @@
 let numbers = prompt('Write 2 numbers, separated by commas');
-let sum = Number(numbers[0]) + Number(numbers[2]);
+let numbersSplit = numbers.split(',');
+let sum = Number(numbersSplit[0]) + Number(numbersSplit[1]);
 console.log(sum);
 
-//works with single digits, doen't work with two-digit numbers etc.
+
+
