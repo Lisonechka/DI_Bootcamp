14895c842973c23230c757e822c21905a08f56c4
Lisonechka <elizza402@gmail.com>
2021-07-25 14:08:37 +0300

    Added solution, ask about bonus

---
+++ Week3/Day4/XPNinja/SecretWord/script.js
@@ -0,0 +1,7 @@
+let secretWord = prompt('Write a secret word');
+console.log(secretWord.replace(/[aeiou]/gi, ''));
+
+//bonus
+// let vowels = [a = 1, e = 2, i = 3, o = 4, u = 5];
+// console.log(secretWord.replace(/[aeiou]/gi, vowels));
+ /// Use loops??
