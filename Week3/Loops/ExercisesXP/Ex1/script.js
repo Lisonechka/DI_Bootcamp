343f9b00ca9e051e1d5c2db1a0d8ef32fe701028
Lisonechka <elizza402@gmail.com>
2021-07-25 14:55:36 +0300

    Added solutionask about bonus

--- Week3/Loops/ExercisesXP/Ex1/script.js
+++ Week3/Loops/ExercisesXP/Ex1/script.js
@@ -1,16 +1,17 @@
 let colors = ["white", "black", "blue", "red", "pink"];
 let number = 1;
 for (let i in colors) {
- console.log("My #" + number++ + " choice is " + colors[i]);
+console.log("My #" + number++ + " choice is " + colors[i]);
 }
 
-// //bonus
-// let colors = ["white", "black", "blue", "red", "pink"];
-// let number = 1;
-// let suffix = ["st", "nd", "rd", "th", "th"];
-// let x = suffix++;
-// for (let i in colors) {
-// console.log("My " + number++ +  suffix[x] + " choice is " +  colors[i]);
+// bonus
+ let colors = ["white", "black", "blue", "red", "pink"];
+ let number = 1;
+ let suffix = ["st", "nd", "rd", "th", "th"];
+ for (let i = 0; i < colors.length; i++ ) {
+     for (let j = 0; j < suffix.length; j++) {
+     }
+     console.log("My " + number++ +  suffix[j] + " choice is " +  colors[i]);
 
-// }    
+ } // don't know, why doesn't work don't understand   
     
