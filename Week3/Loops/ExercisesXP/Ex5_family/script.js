2e7953bd9d0ef0b529729494b411d3a1b6c8e9d3
Lisonechka <elizza402@gmail.com>
2021-07-25 16:25:03 +0300

    Added solution

---
+++ Week3/Loops/ExercisesXP/Ex5_family/script.js
@@ -0,0 +1,13 @@
+
+
+let family = {father:"Jon", mother:"Linda", son:"Shon", daughter:"Wendy"};
+//1
+for (let i in family) {
+    console.log(i);
+ }
+
+ 
+//2
+for (let i in family) {
+    console.log(family[i]);
+}
