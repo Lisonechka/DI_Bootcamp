0adbb38004c346efa99ea5f45dd03e685b3dee27
Lisonechka <elizza402@gmail.com>
2021-07-25 20:33:47 +0300

    Added first steps

---
+++ Week3/Loops/ExercisesXP/Ex7/script.js
@@ -0,0 +1,7 @@
+let names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+names.sort();
+for (let i in names) {
+    i.slice(0,1);
+    // result += firstLetter;
+    console.log(i);
+}
