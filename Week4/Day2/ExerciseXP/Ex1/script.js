81f520fd917db3744f90256af1e8265c966e2068
Lisonechka <elizza402@gmail.com>
2021-07-26 23:33:26 +0300

    Part 1-2

---
+++ Week4/Day2/ExerciseXP/Ex1/script.js
@@ -0,0 +1,13 @@
+//Part I
+function infoAboutMe() {
+    console.log ("My name is Liza. I love surfing.")
+}
+infoAboutMe();
+
+//Part II
+function infoAboutPerson(personName, personAge, personFavoriteColor) {
+    console.log("You name is " + personName + ", " + "you are " + personAge + " years old, " + "your favorit color is " + personFavoriteColor);
+
+}
+infoAboutPerson("David", 45, "blue");
+infoAboutPerson("Josh", 12, "yellow");
