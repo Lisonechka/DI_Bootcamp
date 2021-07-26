fed4a4ddcb28d914c06bee6b2091f6ccf942c0ea
Lisonechka <elizza402@gmail.com>
2021-07-26 22:44:45 +0300

    Added solution, ? with one line

--- Week3/Loops/ExercisesXP/Ex7/script.js
+++ Week3/Loops/ExercisesXP/Ex7/script.js
@@ -1,7 +1,6 @@
 let names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
 names.sort();
 for (let i in names) {
-    i.slice(0,1);
-    // result += firstLetter;
-    console.log(i);
+   firstLetter = names[i][0];
+console.log(firstLetter);
 }
