ca034e7ea13f435cb1b605654a56d1cf2030143b
Lisonechka <elizza402@gmail.com>
2021-07-28 00:12:12 +0300

    don't understand instructions

--- Week4/Day2/ExerciseXP/Ex1/script.js
+++ Week4/Day2/ExerciseXP/Ex1/script.js
@@ -1,13 +1,22 @@
-//Part I
-function infoAboutMe() {
-    console.log ("My name is Liza. I love surfing.")
-}
-infoAboutMe();
+// //Part I
+// function infoAboutMe() {
+//     console.log ("My name is Liza. I love surfing.")
+// }
+// infoAboutMe();
 
-//Part II
-function infoAboutPerson(personName, personAge, personFavoriteColor) {
-    console.log("You name is " + personName + ", " + "you are " + personAge + " years old, " + "your favorit color is " + personFavoriteColor);
+// //Part II
+// function infoAboutPerson(personName, personAge, personFavoriteColor, personHobbies) {
+//     console.log("You name is " + personName + ", " + "you are " + personAge + " years old, " + "your favorit color is " + personFavoriteColor);
+
+// }
+// infoAboutPerson("David", 45, "blue");
+// infoAboutPerson("Josh", 12, "yellow");
+
+//Part III
+function infoAboutPerson(personName, personAge, personFavoriteColor, personHobbies) {
+    personHobbies = [];
 
 }
-infoAboutPerson("David", 45, "blue");
-infoAboutPerson("Josh", 12, "yellow");
+;
+infoAboutPerson("David", 45, "blue", ["tennis", "painting"])
+infoAboutPerson("Josh", 12, "yellow", ["videoGame", "hanging out with friends", "playing cards"])
