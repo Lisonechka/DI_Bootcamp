43fe2e35535ff11a246926ff5cb27f6cd56597d9
Lisonechka <elizza402@gmail.com>
2021-07-28 13:08:42 +0300

    Added Bonus

--- Week4/Day2/ExerciseXP/Ex4/script.js
+++ Week4/Day2/ExerciseXP/Ex4/script.js
@@ -1,3 +1,4 @@
+
 function isDivisible() {
     let result = "";
     let sum = 0;
@@ -8,7 +9,23 @@ function isDivisible() {
         }
     }
     console.log(result);
-    console.log(sum);
+     console.log(sum);
     
-}
-isDivisible()
+ }
+ isDivisible()
+
+//Bonus
+// function isDivisible(divisor) {
+//     let result = "";
+//     let sum = 0;
+//     for (i =0; i <501; i++) {
+//         if (i %divisor === 0) {
+//             result += i + " ";
+//             sum +=i
+//         }
+//     }
+//     console.log(result + " the numbers divisible by " + divisor + ", and their sum is " + sum);
+    
+// }
+// isDivisible(3);
+// isDivisible(45);
