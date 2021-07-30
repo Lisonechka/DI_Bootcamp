87bd318e0643805f584eb1805679f2e08966da07
Lisonechka <elizza402@gmail.com>
2021-07-30 22:17:10 +0300

    try

---
+++ Week4/Remote_learning/ExerciseXP/ex1.js
@@ -0,0 +1,5 @@
+var num = 8;
+var num = 10;
+console.log(num);
+
+//return 10, last value of var
---
+++ Week4/Remote_learning/ExerciseXP/ex2.js
@@ -0,0 +1,11 @@
+function numbers() {
+    for (var i = 0; i < 5; i += 1) {
+      console.log(i);
+    }
+      console.log(i);
+  }
+  numbers();
+
+  //don't understand instructions
+
+  
