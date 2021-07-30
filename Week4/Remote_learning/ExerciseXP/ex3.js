9ec27d8a78acc7db28ce6ac750ef65eeb36c6c6a
Lisonechka <elizza402@gmail.com>
2021-07-30 22:26:39 +0300

    not sure, that understand instruction

---
+++ Week4/Remote_learning/ExerciseXP/ex3.js
@@ -0,0 +1,9 @@
+var amountOfMoney;
+var amountOfVat;
+var amountOfExp;
+var amountOfRev;
+let expByVat = amountOfExp * amountOfVat;
+amountOfMoney = amountOfRev - expByVat;
+console.log(amountOfMoney);
+
+//?
