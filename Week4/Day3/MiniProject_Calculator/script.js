40f27010180d3074d410ada1dc5d79620a2f926d
Lisonechka <elizza402@gmail.com>
2021-07-30 22:14:16 +0300

    tries

---
+++ Week4/Day3/MiniProject_Calculator/script.js
@@ -0,0 +1,21 @@
+
+
+function number(num) {
+
+
+}
+
+
+
+
+function operator(operator) {
+    document.form.textview.value = document.form.textview.value + operator;
+
+}
+
+
+
+
+function equal() {
+
+}
