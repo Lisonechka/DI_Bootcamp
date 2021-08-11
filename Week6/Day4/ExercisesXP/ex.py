f16f729b382d17ea826703ff9235f9c22f928de3
Lisonechka <elizza402@gmail.com>
2021-08-11 22:18:31 +0300

    Added ex3

--- Week6/Day4/ExercisesXP/ex.py
+++ Week6/Day4/ExercisesXP/ex.py
@@ -1,18 +1,26 @@
-# Exercise 1
-#1
-my_fav_numbers = {3, 16, 7, 8}
-#2
-new_numbers = [30, 55]
-my_fav_numbers.update(new_numbers)
-print(my_fav_numbers)
-#3
-my_fav_numbers = set(list(my_fav_numbers)[:-1])
-print(my_fav_numbers)
-#4-5
-friend_fav_numbers = {1, 7, 90, 100}
-our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
-print(our_fav_numbers)
+# # Exercise 1
+# #1
+# my_fav_numbers = {3, 16, 7, 8}
+# #2
+# new_numbers = [30, 55]
+# my_fav_numbers.update(new_numbers)
+# print(my_fav_numbers)
+# #3
+# my_fav_numbers = set(list(my_fav_numbers)[:-1])
+# print(my_fav_numbers)
+# #4-5
+# friend_fav_numbers = {1, 7, 90, 100}
+# our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
+# print(our_fav_numbers)
 
-#Exercise2
+#Exercise 2
 # Only if tuple convert into a list, and then back
+
+#Exercise 3
+item = 1
+for i in range(1, 21):
+    print(item)
+    item +=1
+
+
 
