53ace054a036f72401067eddf9cd412d3e50b8eb
Lisonechka <elizza402@gmail.com>
2021-08-15 18:03:43 +0300

    few more steps

--- Week6/Dictionaries/ExercisesXP/exxp.py
+++ Week6/Dictionaries/ExercisesXP/exxp.py
@@ -6,3 +6,11 @@ print(result)
 
 # Exercise 2
 family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+for key, value in family.items():
+    if value < 3:
+        print(f'For {family[key]} the ticket is free.')
+    if value >= 3 and value < 12 :
+        print(f'For {family[key]} the ticket is $10.')
+    if value >= 12:
+        print(f'For {family[key]} the ticket is $15.')
+
