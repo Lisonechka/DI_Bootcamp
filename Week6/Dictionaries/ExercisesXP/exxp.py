55884381fefb257c442d8695f73ed2047924d6c8
Lisonechka <elizza402@gmail.com>
2021-08-15 16:01:48 +0300

    added exXP 1

---
+++ Week6/Dictionaries/ExercisesXP/exxp.py
@@ -0,0 +1,8 @@
+# Exercise 1
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+result = dict(zip(keys, values))
+print(result)
+
+# Exercise 2
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
