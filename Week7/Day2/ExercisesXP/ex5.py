5430ea89dbc881f99f3a46adf2f713c95a48fefd
Lisonechka <elizza402@gmail.com>
2021-08-16 22:15:52 +0300

    added changes

--- Week7/Day2/ExercisesXP/ex5.py
+++ Week7/Day2/ExercisesXP/ex5.py
@@ -1,5 +1,6 @@
 # Exercise 5 : Let’s Create Some Personalized Shirts !
-def make_shirt(size = 'Large', printed_text = 'I love Python'):
+def make_shirt(size, printed_text = 'I love Python'):
     print(f'The size of the shirt is {size} and message is {printed_text}')
 
-make_shirt()
+make_shirt("M")
+make_shirt('L')
