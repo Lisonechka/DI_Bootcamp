829806da9288cee53e5ecbcc643eea835a35a54d
Lisonechka <elizza402@gmail.com>
2021-08-17 14:40:28 +0300

    solution for ex6

---
+++ Week7/Day2/ExercisesXP/ex6.py
@@ -0,0 +1,8 @@
+# Exercise 6 : Magicians …
+magicians_names = ['John', 'Albert', 'Frederick', 'Paolo', 'Ludwig']
+def show_magicians():
+    for mag in magicians_names:
+        print([mag])
+
+
+show_magicians()
