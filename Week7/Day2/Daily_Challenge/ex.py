9bac6c0bce6c62ea07aafd70c386687ea5a50fb3
Lisonechka <elizza402@gmail.com>
2021-08-17 15:17:16 +0300

    final solution ex6

---
+++ Week7/Day2/Daily_Challenge/ex.py
@@ -0,0 +1,10 @@
+# Solve The Matrix
+matrix_text = 
+    7i3
+    Tsi
+    h%x
+    i #
+    sM 
+    $a 
+    #t%
+    ^r!
--- Week7/Day2/ExercisesXP/ex6.py
+++ Week7/Day2/ExercisesXP/ex6.py
@@ -4,5 +4,10 @@ def show_magicians():
     for mag in magicians_names:
         print([mag])
 
+def make_great():
+    for key, value in enumerate(magicians_names):
+        magicians_names[key] = 'the Great ' + value
 
+make_great()
+print([magicians_names])
 show_magicians()
