0464d1bc1a3885d8cd25bb531f7bc477fc4701a1
Lisonechka <elizza402@gmail.com>
2021-08-18 12:48:16 +0300

    solution

--- Week7/Day2/Daily_Challenge/ex.py
+++ Week7/Day2/Daily_Challenge/ex.py
@@ -1,10 +1,23 @@
 # Solve The Matrix
-matrix_text = 
-    7i3
-    Tsi
-    h%x
-    i #
-    sM 
-    $a 
-    #t%
-    ^r!
+matrix_text = [
+    ['7', 'i', '3'],
+    ['T', 's', 'i'],
+    ['h', '%', 'x'],
+    ['i', ' ' , '#'],
+    ['s', 'M', ' '],
+    ['$', 'a', ' '],
+    ['#', 't', '%'],
+    ['^', 'r', '!']
+    ]
+result = ''
+num_of_column = len(matrix_text[0])
+num_of_rows = len(matrix_text)
+for i in range(num_of_column):
+	for j in range(num_of_rows):
+		# scan the column
+		if matrix_text[j][i].isalpha():
+			result += matrix_text[j][i]
+		elif result and result[-1] != ' ':  # result is not an empty string and the last item is not a space
+			result += ' '
+
+print(result)
