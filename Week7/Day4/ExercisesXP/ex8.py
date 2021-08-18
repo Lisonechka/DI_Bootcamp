eb71e106171dab3fb9441e1b31b7a713bf69db92
Lisonechka <elizza402@gmail.com>
2021-08-19 00:34:04 +0300

    ex8 first try

---
+++ Week7/Day4/ExercisesXP/ex8.py
@@ -0,0 +1,13 @@
+# Exercise 8
+
+# def crosses(num = int):
+#     n=1
+#     for num in num:
+#         for i in range(1,5):
+#         n+=1
+#         print(i*n)
+# crosses(2)
+
+# print(int(str(3)*2) + int(str(3)*2))
+
+
