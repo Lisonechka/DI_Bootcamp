200e954ff5d0f88c3ec2ec51a1f93ce834055837
Lisonechka <elizza402@gmail.com>
2021-08-22 17:03:46 +0300

    have bugs

--- Week7/Day5/TicTacToe/ex.py
+++ Week7/Day5/TicTacToe/ex.py
@@ -1,15 +1,56 @@
 # Mini-Project - Tic Tac Toe
 
-board = [
-	[' ', ' ', ' '],
-	[' ', ' ', ' '],
-	[' ', ' ', ' ']
-]
+board = range(1,10)
+def display_board(board):
+	print('-------------')
+	for i in range(3):
+		print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+		print("-------------")
 
-def display_board():
-	print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-	print("--+---+--")
-	print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-	print("--+---+--")
-	print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-display_board()
+def player_input(player_token):
+    valid = False
+    while not valid:
+        player_answer = input("Where do you put " + player_token+ "?")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Invalid input. Are you sure you entered a number?")
+            continue
+        if player_answer >= 1 and player_answer <= 9:
+            if (str(board[player_answer-1]) not in "XO"):
+                board[player_answer-1] = player_token
+                valid = True
+            else:
+                print("This cell is already taken")
+        else:
+            print("Invalid input. Enter a number from 1 to 9 to be like.")
+
+def check_win(board):
+    win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
+
+def play(board):
+    counter = 0
+    win = False
+    while not win:
+        display_board(board)
+        if counter % 2 == 0:
+            player_input("X")
+        else:
+            player_input("O")
+        counter += 1
+        if counter > 4: # in order to avoid the unnecessary call to the check_win function (no one can win for sure until the fifth move).
+            tmp = check_win(board) # the same reason. We just remember value and, if necessary, use it
+            if tmp:
+                print (tmp, "is won!")
+                win = True
+                break
+        if counter == 9:
+            print ("dead heat!")
+            break
+    display_board(board)
+
+play(board)
