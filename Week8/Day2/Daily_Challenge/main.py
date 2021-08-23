480674f53a398260cc0dba9e7863a61c0e3c4db4
Lisonechka <elizza402@gmail.com>
2021-08-23 23:50:31 +0300

    added first steps

---
+++ Week8/Day2/Daily_Challenge/main.py
@@ -0,0 +1,31 @@
+class Farm:
+
+    def __init__(self, name_of_farm):
+        self.name = name_of_farm
+        self.animals = {}
+
+    def add_animal(self, animal='', amount=1):
+        if animal not in self.animals:
+            print(f'{animal}: {amount}')
+        else:
+            print(f'{animal}: {amount + 1}')
+
+    def get_info(self):
+        print(f"{self.name}'s farm")
+        self.add_animal()
+
+
+macdonald = Farm("McDonald")
+macdonald.add_animal('cow', 5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
+print(macdonald.get_info())
+
+# McDonald's farm
+#
+# cow : 5
+# sheep : 2
+# goat : 12
+
+# E-I-E-I-0!
