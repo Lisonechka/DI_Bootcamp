1c93bd5d16b5dc544d727e6120701362382e4aa4
Lisonechka <elizza402@gmail.com>
2021-08-24 19:02:14 +0300

    solved

--- Week8/Day2/Daily_Challenge/main.py
+++ Week8/Day2/Daily_Challenge/main.py
@@ -1,31 +1,29 @@
+from typing import List
+
 class Farm:
 
-    def __init__(self, name_of_farm):
+    def __init__(self, name_of_farm: str):
         self.name = name_of_farm
         self.animals = {}
 
-    def add_animal(self, animal, amount=1):
-        if animal not in self.animals:
-            print(f'{animal}: {amount}')
-        else:
-            print(f'{animal}: {amount + 1}')
+    def add_animal(self, animal: str, amount: int = 1) -> None:
+        self.animals[animal] = self.animals.get(animal, 0) + amount
 
-    def get_info(self):
-        print(f"{self.name}'s farm")
+    def get_info(self) -> None:
+        print(f"{self.name}'s farm\n")
+        for animal_name, animal_count in self.animals.items():
+            print(f"{animal_name}: {animal_count}")
+        print("\tE-I-E-I-0!")
 
-
-macdonald = Farm("McDonald")
-print(macdonald.get_info())
-macdonald.add_animal('cow', 5)
-macdonald.add_animal('sheep')
-macdonald.add_animal('sheep')
-macdonald.add_animal('goat', 12)
+    def get_animal_types(self) -> List:
+        return sorted(self.animals.keys())
 
 
-# McDonald's farm
-#
-# cow : 5
-# sheep : 2
-# goat : 12
+farm = Farm('McDonald')
+farm.add_animal('cow',5)
+farm.add_animal('sheep')
+farm.add_animal('sheep')
+farm.add_animal('goat', 12)
 
-# E-I-E-I-0!
+farm.get_info()
+print(farm.get_animal_types())
