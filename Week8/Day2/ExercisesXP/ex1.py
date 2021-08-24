120d63df66d58fca16f333715139bdf31ac48cf5
Lisonechka <elizza402@gmail.com>
2021-08-24 23:51:19 +0300

    solved

--- Week8/Day2/ExercisesXP/ex1.py
+++ Week8/Day2/ExercisesXP/ex1.py
@@ -1,13 +1,21 @@
 # Exercise 1: Cats
+from typing import List
+
 
 class Cat:
-    def __init__(self, name, age):
+    def __init__(self, name=List, age=List):
         self.name = name
         self.age = age
 
-cat1 = Cat('Big', 3)
-cat2 = Cat('Toto', 1)
-cat3 = Cat('Manu', 5)
 
-oldest_cat = max(cat1.age, cat2.age, cat3.age)
-print(oldest_cat)
+cats = Cat(["Big", "Toto", "Manu"], [3, 1, 5])
+
+
+def oldest_cat():
+    the_oldest_cat = max(cats.age)
+    oldest_idx = cats.age.index(the_oldest_cat)
+    oldest_name = cats.name[oldest_idx]
+    return oldest_name
+
+
+print(f'The oldest cat is {oldest_cat()}, and is {max(cats.age)} years old.')
