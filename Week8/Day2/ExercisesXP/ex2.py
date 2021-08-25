6bded9bb91872160fa09c9ca08fa06f982a72d8b
Lisonechka <elizza402@gmail.com>
2021-08-25 22:23:00 +0300

    solved ex3

--- Week8/Day2/ExercisesXP/ex2.py
+++ Week8/Day2/ExercisesXP/ex2.py
@@ -1,6 +1,31 @@
 # Exercise 2 : Dogs
 
 class Dog:
-    def __init__(self, name=str, height=int):
+    def __init__(self, name, height):
         self.name = name
         self.height = height
+
+    def bark(self):
+        print(f'{self.name} goes woof!')
+
+    def jump(self):
+        x = self.height * 2
+        print(f'{self.name} jumps {x} cm high!')
+
+
+davids_dog = Dog("Rex", 50)
+print(davids_dog.name)
+print(davids_dog.height)
+davids_dog.bark()
+davids_dog.jump()
+
+sarahs_dog = Dog('Teacup', 20)
+print(sarahs_dog.name)
+print(sarahs_dog.height)
+sarahs_dog.bark()
+sarahs_dog.jump()
+
+if davids_dog.height > sarahs_dog.height:
+    print(f'{davids_dog.name} is bigger')
+else:
+    print(f'{sarahs_dog.name} is bigger')
