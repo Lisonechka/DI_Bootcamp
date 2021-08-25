2ece2fa9e9ee500978f15745523846f9e9b1fdc9
Lisonechka <elizza402@gmail.com>
2021-08-25 23:10:51 +0300

    try

---
+++ Week8/Day4/Daily_challenge/main.py
@@ -0,0 +1,24 @@
+# Daily Challenge: DNA
+from random import randint
+
+
+class Genes:
+    def __init__(self):
+        self.value_gene = randint(0, 2)
+
+
+class Chromosome(Genes):
+    def __init__(self):
+        super().__init__():
+
+
+class DNA(Chromosome):
+
+
+
+
+
+
+
+
+
