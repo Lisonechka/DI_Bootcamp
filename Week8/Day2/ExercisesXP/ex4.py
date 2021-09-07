68a2aeab0767ec6cf71670c37adbfa05de75a5b3
Lisonechka <elizza402@gmail.com>
2021-09-07 22:26:14 +0300

    added some solution

---
+++ Week8/Day2/ExercisesXP/ex4.py
@@ -0,0 +1,35 @@
+# Exercise 4 : Afternoon At The Zoo
+class Zoo:
+    def __init__(self, zoo_name: str):
+        self.animals = []
+        self.name = zoo_name
+
+    def add_animal(self, new_animal):
+        if new_animal not in self.animals:
+            self.animals.append(new_animal)
+        else:
+            print(' already exist')
+
+    def get_animals(self):
+        print(self.animals)
+
+    def sell_animal(self, animal_sold):
+        self.animals.remove(animal_sold)
+
+    def sort_animals(self):
+        print(dict(enumerate(self.animals)))
+
+
+zoo1 = Zoo("Moonlight")
+zoo1.add_animal('Ape')
+zoo1.add_animal('Cat')
+zoo1.add_animal('Baboon')
+zoo1.add_animal("Bear")
+zoo1.add_animal('Eel')
+zoo1.add_animal('Cougar')
+zoo1.add_animal('Emu')
+zoo1.get_animals()
+
+zoo1.sell_animal("Bear")
+zoo1.get_animals()
+zoo1.sort_animals()
